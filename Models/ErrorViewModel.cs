6828dbe415bba4bb011e43fecc799059794d58a3
esg-training <46299267+esg-training@users.noreply.github.com>
2020-03-22 11:30:17 +0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASEME_Dashboard_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
